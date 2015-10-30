8a682e8d0d342f27ffa93123b698406b284bebc0
glebec <gvorotilov@gmail.com>
2015-10-30 23:57:45 +0300

    примитивный роутинг

--- index.php
+++ index.php
@@ -4,4 +4,19 @@ require __DIR__.'/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__.'/views');
 $twig = new Twig_Environment($loader, ['cache' => false]);
 
-echo $twig->render('homepage.html', ['message' => 'Привет чувак!']);
+$request = $_SERVER['REQUEST_URI'];
+switch($request){
+    case '/home':
+        echo $twig->render('homepage.html', ['message' => 'Привет чувак!']);
+        break;
+    case '/about':
+        echo $twig->render('about.html', ['message' => 'О нас']);
+        break;
+    case '/products':
+        echo $twig->render('products.html', ['message' => 'Наши изделия']);
+}
+/*
+if ($request == '/home'){
+    echo $twig->render('homepage.html', ['message' => 'Привет чувак!']);
+}
+*/
