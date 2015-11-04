1eb63139e4704a90743d6f9c4d17f422d71fcf72
glebec-P67A <gvorotilov@gmail.com>
2015-11-04 18:43:50 +0300

    работает выборка по id товара

--- classes/DataProvider.php
+++ classes/DataProvider.php
@@ -8,6 +8,14 @@ class DataProvider
     public function getHome() { return $this->home; }
     public function getAbout() { return $this->about; }
     public function getProducts() { return $this->products; }
+    public function getItem($id)
+    {
+        $this->products->rewind();
+        while ($this->products->valid() && $this->products->key() != ($id-1)){
+            $this->products->next();
+        }
+        return $this->products->current();
+    }
 
     public static function getInstance()
     {
@@ -107,30 +115,35 @@ class DataProvider
         $ship_M50 = new stdClass();
         $ship_890J = new stdClass();
 
+        $ship_300i->id = 1;
         $ship_300i->name = '300i';
         $ship_300i->type = 'common';
         $ship_300i->image = '/views/pics/300i_beauty_visual.jpg';
         $ship_300i->description = 'The 300i touring model is the premiere spacecraft hull on the market today. A true
                                     jack-of-all-trades, the 300i is capable of making any role its own and doing so with the class and sophistication
                                      expected of an ORIGIN design. All models feature Gorgon Defender shields and A&R Omnisky VI cannon standard.';
+        $ship_315p->id = 2;
         $ship_315p->name = '315p';
         $ship_315p->type = 'exploration';
         $ship_315p->image = '/views/pics/315p_flight_visual.jpg';
         $ship_315p->description = 'Exploration is man’s highest calling. Prepare to chart distant horizons with man’s most sophisticated piece of technology,
                                     the ORIGIN 315p. Featuring a more robust power plant and a custom scanning package, exclusively designed by Chimera
                                      Communications, the 315p is designed for the pilot who wants to go further, to see things that few have seen.';
+        $ship_325a->id = 3;
         $ship_325a->name = '325a';
         $ship_325a->type = 'interdiction';
         $ship_325a->image = '/views/pics/325a_front-Right_visual.jpg';
         $ship_325a->description = 'It’s a dangerous universe out there. Be the arbiter of your fate with the combat-enhanced ORIGIN 325a. Just because it’s a rough
                                     galaxy doesn’t mean you need to sacrifice your comfort: the 325a can come out on top in any dogfight. The 325a features an
                                      advanced weapon payload as well as a custom targeting system designed especially for the 325a by WillsOp.';
+        $ship_M50->id = 4;
         $ship_M50->name = 'M50';
         $ship_M50->type = 'racing, interceptor';
         $ship_M50->image = '/views/pics/M50_comp52.jpg';
         $ship_M50->description = 'If you want to get from point A to point B as quickly as possible and with as much style as
                                    possible then ORIGIN\'s M50 is for you. Featuring supercharged engines that counter a tiny weapons loadout, the M50 is
                                     a ship for going FAST.';
+        $ship_890J->id = 5;
         $ship_890J->name = '890 Jump';
         $ship_890J->type = 'Luxury touring';
         $ship_890J->image = '/views/pics/890_Beautyshot-World_v01_NF_140925.jpg';
--- classes/ProductsController.php
+++ classes/ProductsController.php
@@ -2,13 +2,32 @@
 
 class ProductsController implements IController
 {
+    protected $twig, $data, $fc;
+
+    public function __construct()
+    {
+        $this->twig = TwigFacade::getInstance();
+        $this->data = DataProvider::getInstance();
+        $this->fc = FrontController::getInstance();
+    }
+
     public function indexAction()
     {
-        $fc = FrontController::getInstance();
-        $twig = TwigFacade::getInstance();
-        $data = DataProvider::getInstance();
-        $storage = $data->getProducts();
-        $result = $twig->render('products.twig', ['storage' => $storage]);
-        $fc->setBody($result);
+        $storage = $this->data->getProducts();
+        $result = $this->twig->render('products.twig', ['storage' => $storage]);
+        $this->fc->setBody($result);
+    }
+
+    public function showAction()
+    {
+        $params = $this->fc->getParams();
+        if (count($params) == 1 ) {
+            $id = (int)$params['id'];
+            $item = $this->data->getItem($id);
+        }else{
+            $item = null;
+        }
+        $result = $this->twig->render('products_show_by_id.twig', ['item' => $item]);
+        $this->fc->setBody($result);
     }
 }
